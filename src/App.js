d3ff520d32bf007609cbe47e09ad93de017ee8af
codixir <codixir@gmail.com>
2019-04-08 08:29:12 -0400

    Handle delete, edit and input changes. Refactor code.

--- src/App.js
+++ src/App.js
@@ -20,14 +20,35 @@ class App extends Component {
     }
 
     this.state.handleSubmit = this.handleSubmit.bind(this);
+    this.state.handleDelete = this.handleDelete.bind(this);
+    this.state.handleEdit = this.handleEdit.bind(this);
     this.state.handleInputChange = this.handleInputChange.bind(this);   
-    this.setRef = this.setRef.bind(this); 
-  }
-  
-  setRef(input) {
-    this.childRef = input;
   }
 
+  handleDelete(book) {
+    let idx = -1;
+
+    for (let i = 0; i < this.state.books.length; i++) {
+      if (this.state.books[i].id === book.id) {
+        idx = i;
+        break;
+      }
+    }
+
+    this.state.books.splice(idx, 1);
+    this.setState({books: this.state.books});
+  }
+
+  handleEdit(book) {
+    for (let key in this.state.book) {
+      if (this.state.book.hasOwnProperty(key)) {
+        this.state.book[key] = book[key];
+      }
+    }
+
+    this.setState({book: this.state.book});
+  }
+ 
   handleValidation() {
     let errors = {};
 
@@ -56,29 +77,52 @@ class App extends Component {
 
     this.setState({errorMessage: ''});
 
-    this.state.book.id = this.state.books.length + 1;
-    this.setState({
-      book: { 
-        title: this.state.book.title,
-        author: this.state.book.author,
-        year: this.state.book.year,
-      }
-    }); 
+    if (this.state.book.id) {
+      let idx = -1;
 
-    this.setState({
-      books: this.state.books.concat(this.state.book),
-      book: { 
-        title: '',
-        author: '',
-        year: '',
+      for (let i = 0; i < this.state.books.length; i++) {
+        if (this.state.books[i].id === this.state.book.id) {        
+          this.state.books[i] = this.state.book;
+          break;
+        }
       }
-    });    
+      
+      this.setState({books: this.state.books});
+      this.setState({ 
+        book: { 
+          id: 0,
+          title: '',
+          author: '',
+          year: '',
+        }
+      });
+    } else {      
+      let id = this.state.books[this.state.books.length - 1].id;
 
-    this.childRef.reset();
+      this.state.book.id = id + 1;
+      this.setState({
+        book: { 
+          title: this.state.book.title,
+          author: this.state.book.author,
+          year: this.state.book.year,
+        }
+      }); 
+  
+      this.setState({
+        books: this.state.books.concat(this.state.book),
+        book: { 
+          id: 0,
+          title: '',
+          author: '',
+          year: '',
+        }
+      });   
+    } 
   }
 
-  handleInputChange(e) {      
+  handleInputChange(e) {        
     this.state.book[e.target.name] = e.target.value;
+    this.setState({book: this.state.book});
   }
 
   componentWillMount() {
@@ -94,11 +138,14 @@ class App extends Component {
               book={this.state.book }
               errorMessage = {this.state.errorMessage}
               handleSubmit={this.state.handleSubmit}
-              handleInputChange={this.state.handleInputChange}
-              setRef={this.setRef}
+              handleInputChange={this.state.handleInputChange}      
           />
           <br></br><hr></hr>          
-          <Books books={this.state.books} />
+          <Books 
+            books={this.state.books}
+            handleDelete={this.state.handleDelete}
+            handleEdit={this.state.handleEdit}
+          />
         </div>        
       </div>
     );
